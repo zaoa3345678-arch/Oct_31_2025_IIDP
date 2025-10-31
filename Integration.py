c93db3374c1c6e78416c3101c2faa9d047d57258
Aaron HYM <hyma128@pu.edu.tw>
2025-10-31 17:28:11 +0800

    build function delete_student

--- Integration.py
+++ Integration.py
@@ -64,6 +64,13 @@ def overview_student():
 botton_overview = tk.Button(root, text='Overview', command=overview_student)
 botton_overview.pack(pady=25)
 
-print ('hello software')
+def delete_student():
+    student_id = entry_id.get()
+    cursor.execute('SELECT * from DB_student where db_student_id = ?',(student_id,))
+    delete = cursor.fetchall()
+    cursor.execute('DELETE from DB_student where db_student_id = ?',(student_id,))
+    print ('Following row is delete:', delete)
+    conn.commit()
+
 
 root.mainloop() #must be put to the end of programming code
